54b6f0b349ec73efcad79f93fe4c6025b3b21884
Hawkie <paulhawkins123@gmail.com>
2018-10-29 18:27:02 +0000

    build 1

---
+++ SolverLib/SolverLib/Algorithms/ISetTester.cs
@@ -0,0 +1,9 @@
+﻿using SolverLib.Core;
+
+namespace SolverLib.Algorithms
+{
+    public interface ISetTester<TKey>
+    {
+        bool Test<TKey>(Keys<TKey> currentCombination, Keys<TKey> set);
+    }
+}
